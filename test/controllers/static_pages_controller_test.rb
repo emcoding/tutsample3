28b5fe71549603548e20b080d1869e26935fdbf6
F3PiX <f3pixnl@gmail.com>
2015-04-30 22:29:04 +0200

    Adding html to pages, and html page titles, tests passing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,20 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Mauds Rails App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Mauds Rails App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Mauds Rails App"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Mauds Rails App"
  end
 end
