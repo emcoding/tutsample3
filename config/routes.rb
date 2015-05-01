f1cadebc9b9ac2a6d9ba00e159487f12299a4dcc
F3PiX <f3pixnl@gmail.com>
2015-05-01 22:22:23 +0200

    Replacing stubbed routing # by named routes in _header and _footer layout files. Commenting out log in menu item

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # get 'static_pages/home' (which is an url, replaced by controller#action pair:)
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact' => 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
