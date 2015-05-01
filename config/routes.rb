cddbdcc0ef61ab29e25d35ce719815e6753f00c5
F3PiX <f3pixnl@gmail.com>
2015-05-01 23:14:53 +0200

    Adding users controller plus adding all links (with signup_path).

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  # get 'static_pages/home' (which is an url, replaced by controller#action pair:)
+  get 'users/new'
+
+  # get 'static_pages/home' (which is an url) replaced by controller#action pair:)
   root              'static_pages#home'
 
   get 'help'    =>  'static_pages#help'
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
   get 'about'   =>  'static_pages#about'
 
   get 'contact' =>  'static_pages#contact'
+
+  get 'signup'  =>  'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
