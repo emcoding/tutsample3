ba2e7dbeb4cbf58bf4aa4d738161fdfeee0c5122
F3PiX <f3pixnl@gmail.com>
2015-05-05 09:42:41 +0200

    Adding create method to users controller; adding view and css to show signup form on signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
 
-  def new
-  end
 
   def show
     @user = User.find(params[:id])
-    #µd: with debugger in action, go to terminal to use it. 
+    #µd: with debugger in action, go to terminal to use it.
     #debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #handle this
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
+    end
 
 end
