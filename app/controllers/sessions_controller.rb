11e51b6b58584796c9909bae91aeb190f133055b
F3PiX <f3pixnl@gmail.com>
2015-05-07 15:33:57 +0200

    Adding login test for invalid logins. rails s

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #created after including SessionHelper in AppController, and def log_in method
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid combination'
+
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+
+  def log_in(user)
+    #this session method is a Rails method. It places a temp cookie which expires upon closing the browser
+    session[:user_id] = user.id
+  end
+
 end
