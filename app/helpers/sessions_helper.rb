674179e6958030275998208dbfb0406a47075691
F3PiX <f3pixnl@gmail.com>
2015-05-07 16:03:11 +0200

    Implementing logging in current_user with log_in function and finding logged in user (ch8.2)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #return the current logged-in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
